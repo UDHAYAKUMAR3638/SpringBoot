6f571a0d541214c3b496438e893ba75c35af71b2
Arulmozhi Karunagaran <saruarul154@gmail.com>
2023-12-30 10:27:58 +0530

    Exception handler class

--- SpringBoot/src/main/java/com/example/springboot/controller/loginController.java
+++ SpringBoot/src/main/java/com/example/springboot/controller/loginController.java
@@ -1,7 +1,4 @@
 package com.example.springboot.controller;
-
-import javax.naming.NameNotFoundException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +8,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.springboot.service.loginService;
 
 @RestController
@@ -19,12 +15,14 @@ import com.example.springboot.service.loginService;
 public class loginController {
     @Autowired
     loginService service;
+
     @PutMapping("/{username}/{pwd}") 
     String key(@PathVariable String username,@PathVariable String pwd) throws NullPointerException
     {  //throw new NameNotFoundException();
         //throw new NullPointerException();
         return service.generateToken(username,pwd);
     }
+
       @GetMapping("/verify/{username}")
     public ResponseEntity<String> tokenValidation(@RequestHeader("Authorization") String authorizationHeader,@PathVariable String username) throws RuntimeException{
         //throw new RuntimeException();
